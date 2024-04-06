d47bbf98f8d0724e0445456f577d98a2706fd2bd
user <user@DESKTOP-7N5VEK9>
2024-04-06 11:55:51 +0800

    加入專案檔案。

---
+++ SecWagore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecWagore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

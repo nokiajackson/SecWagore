1f2503f14cdce5a19ced81fa6644395d16ac8939
jackson liu <nokiajackson@gmail.com>
2024-04-28 21:10:46 +0800

    修改跳頁 和 port try to add swagger

---
+++ SecWagore/Models/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace SecWagore.Models
+{
+    internal class Contact
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ SecWagore/Models/Info.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.OpenApi.Models;
+using System.ComponentModel;
+
+namespace SecWagore.Models
+{
+    internal class Info : OpenApiInfo
+    {
+        public string Title { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+        public string TermsOfService { get; set; }
+        public object Contact { get; set; }
+        public License License { get; set; }
+    }
+}

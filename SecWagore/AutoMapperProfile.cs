904263e6bf1dd17aa926a5cc14e7f1151dd30a5d
jackson liu <nokiajackson@gmail.com>
2024-07-14 10:53:09 +0800

    AutoMapper

--- SecWagore/AutoMapperProfile.cs
+++ SecWagore/AutoMapperProfile.cs
@@ -7,33 +7,13 @@ public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
+        // Mapping from EntryLogVM to EntryLog
+        CreateMap<EntryLogVM, EntryLog>()
+            .ForMember(dest => dest.Purpose, opt => opt.MapFrom(src => (int)src.Purpose));
+
         // Mapping from EntryLog to EntryLogVM
         CreateMap<EntryLog, EntryLogVM>()
             .ForMember(dest => dest.Purpose, opt => opt.MapFrom(src => (Purpose)src.Purpose));
 
-        // Mapping from EntryLogVM to EntryLog
-        CreateMap<EntryLogVM, EntryLog>()
-            .ForMember(dest => dest.Purpose, opt => opt.MapFrom(src => (int)src.Purpose));
-    }
-    private Purpose MapStringToEnum(string purpose)
-    {
-        if (Enum.TryParse<Purpose>(purpose, out var result))
-        {
-            return purpose switch
-            {
-                "洽公" => Purpose.洽公,
-                "送貨" => Purpose.送貨,
-                "活動" => Purpose.活動,
-                "維護" => Purpose.維護,
-                "志工" => Purpose.志工,
-                "面試" => Purpose.面試,
-                "其他" => Purpose.其他,
-                _ => throw new ArgumentException($"無效的 Purpose 值: {purpose}")
-            };
-        }
-        else
-        {
-            throw new ArgumentException($"無效的 Purpose 值: {purpose}");
-        }
     }
 }

3ff62867733c948df631fee867de8548e81c2ffa
jackson liu <nokiajackson@gmail.com>
2024-07-19 00:35:23 +0800

    update

---
+++ SecWagore/webpack.config.js
@@ -0,0 +1,55 @@
+﻿const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        vendor: [
+            'bootstrap',
+            'bootstrap-datepicker',
+            'axios',
+            'moment'
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'wwwroot/js'),
+        publicPath: '/js/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // 你的 HTML 模板文件路径
+            filename: '../index.html', // 生成的 HTML 文件路径
+            inject: 'body'
+        })
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
+};
